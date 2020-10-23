eb6ae7e42de64dd9ddde9b5248d8ae94dc99bbe2
azharzulkifli98 <azharzulkifli98@gmail.com>
2020-10-22 21:57:07 -0500

    testing .gitignore

--- CPS Game v3/Assets/Scripts/LevelManager.cs
+++ CPS Game v3/Assets/Scripts/LevelManager.cs
@@ -6,7 +6,7 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // just a filler line
     }
 
     // Update is called once per frame
